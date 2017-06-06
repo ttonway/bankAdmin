d6c4cabc75833ede419793307f3621497e0763b2
tangfeng <tang.feng@wcare.cn>
2017-06-06 16:03:52 +0800

    update

--- src/main/java/com/psbc/controller/AdminUserController.java
+++ src/main/java/com/psbc/controller/AdminUserController.java
@@ -1,12 +1,21 @@
 package com.psbc.controller;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,11 +24,17 @@ import com.psbc.service.AdminUserService;
 import com.psbc.util.MD5Util;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/adminUser")
 public class AdminUserController {
-	
+	private static Logger logger = Logger.getLogger(AdminUserController.class);
+
+	@Value("${service.loan0.url}")
+	private String loan0Url;
+	@Value("${service.loan1.url}")
+	private String loan1Url;
 
 	@Autowired
 	private AdminUserService adminUserService;
@@ -39,6 +54,7 @@ public class AdminUserController {
 			map.put("code", 0);
 			return map;
 		} catch (Exception e) {
+			logger.error("getUserList fail.", e);
 			map.put("code", 1);
 			return map;
 		}
@@ -69,6 +85,7 @@ public class AdminUserController {
 			}
 			return map;
 		} catch (Exception e) {
+			logger.error("submitUser fail.", e);
 			map.put("code", 1);
 			return map;
 		}
@@ -94,6 +111,7 @@ public class AdminUserController {
 			map.put("code", 0);
 			return map;
 		} catch (Exception e) {
+			logger.error("submitReset fail.", e);
 			map.put("code", 1);
 			return map;
 		}
@@ -108,8 +126,28 @@ public class AdminUserController {
 			map.put("code", 0);
 			return map;
 		} catch (Exception e) {
+			logger.error("deleteUser fail.", e);
 			map.put("code", 1);
 			return map;
 		}
-	}	
+	}
+
+	@RequestMapping(value = "/qrcode/{userCode}/{loanType}")
+	public void qrcode(HttpServletResponse response, @PathVariable String userCode, @PathVariable String loanType, Integer w, Integer h) {
+		int width = w == null ? 200 : w.intValue();
+		int height = h == null ? 200 : h.intValue();
+
+		String url = null;
+		loanType.equals("0")
+
+		try {
+			OutputStream stream = response.getOutputStream();
+			QRCodeWriter writer = new QRCodeWriter();
+			BitMatrix m = writer.encode(url, BarcodeFormat.QR_CODE, height, width);
+			MatrixToImageWriter.writeToStream(m, "png", stream);
+			stream.flush();
+		} catch (IOException e) {
+			logger.error("qrcode fail.", e);
+		}
+	}
 }
--- src/main/java/com/psbc/controller/PosterController.java
+++ src/main/java/com/psbc/controller/PosterController.java
@@ -1,30 +1,33 @@
 package com.psbc.controller;
 
-import com.psbc.pojo.AdminUser;
 import com.psbc.pojo.PosterImage;
-import com.psbc.service.AdminUserService;
 import com.psbc.service.PosterService;
-import com.psbc.util.MD5Util;
-import org.apache.commons.lang3.StringUtils;
+import com.psbc.util.Utils;
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/poster")
 public class PosterController {
+	private static Logger logger = Logger.getLogger(PosterController.class);
 
 	@Value("${fileserver.path}")
 	private String fileRootPath;
-
-	@Value("${fileserver.httpurl}")
-	private String fileRootUrl;
 
 	@Autowired
 	private PosterService posterService;
@@ -39,9 +42,75 @@ public class PosterController {
 			map.put("data", list);
 			return map;
 		} catch (Exception e) {
+			logger.error("query poster list fail.", e);
 			map.put("code", 1);
 			return map;
 		}
 	}
 
+    @RequestMapping("upload")
+    public Map<String, Object> upload(MultipartFile file, String posterName) {
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        if (!file.isEmpty()) {
+            try {
+                UUID uuid = UUID.randomUUID();
+                File target = new File(fileRootPath, uuid.toString());
+                file.transferTo(target);
+
+                PosterImage poster = new PosterImage();
+                poster.setFileName(uuid.toString());
+                poster.setPosterName(posterName);
+                poster.setContentType(file.getContentType());
+                String timeStr = Utils.currentTimeStr();
+                poster.setCreatetime(timeStr);
+                poster.setUpdatetime(timeStr);
+                if (posterService.insert(poster) == 1) {
+                    map.put("code", 0);
+                } else {
+                    map.put("code", 1);
+                }
+            } catch (Exception e) {
+                logger.error("upload fail.", e);
+                map.put("code", 1);
+            }
+        }
+        return map;
+    }
+
+    @RequestMapping("/delete")
+    @ResponseBody
+    public Map<String, Object> delete(Long posterId) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        try {
+            posterService.deleteByPrimaryKey(posterId);
+            map.put("code", 0);
+            return map;
+        } catch (Exception e) {
+            map.put("code", 1);
+            return map;
+        }
+    }
+
+    @RequestMapping(value = "/file/{fileName}")
+	public void file(HttpServletResponse response, @PathVariable String fileName) throws IOException {
+		File file = new File(fileRootPath, fileName);
+		logger.debug("read file " + file);
+
+		FileInputStream fis = null;
+		response.setContentType(MediaType.IMAGE_PNG.toString());
+		try {
+			OutputStream out = response.getOutputStream();
+			fis = new FileInputStream(file);
+			byte[] b = new byte[fis.available()];
+			fis.read(b);
+			out.write(b);
+			out.flush();
+		} catch (FileNotFoundException e) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			logger.error("read " + file + " fail.", e);
+		} finally {
+			IOUtils.closeQuietly(fis);
+		}
+	}
 }
